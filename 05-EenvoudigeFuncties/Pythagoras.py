248f42da3046c50b0e5a1b4ffe0f522283c19821
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2018-10-09 17:54:11 +0200

    Pythagoras

--- 05-EenvoudigeFuncties/Pythagoras.py
+++ 05-EenvoudigeFuncties/Pythagoras.py
@@ -1,11 +1,11 @@
 from math import sqrt
 
 #invoer
-a = round(float(input('Geef een getal a groter dan 0: ')), 2)
-b = round(float(input('Geef een getal b groter dan 0: ')), 2)
+a = float(input('Geef een getal a groter dan 0: '))
+b = float(input('Geef een getal b groter dan 0: '))
 
 #bewerking
-c = round(float(sqrt((a ** 2) + (b ** 2))), 2)
+c = float(sqrt((a ** 2) + (b ** 2)))
 
 #uitvoer
-print('In een rechthoekige driehoek met rechthoekszijden a = ' + str(a) + ' en b = ' + str(b) + ' is de schuine zijde ' + str(c))
+print('In een rechthoekige driehoek met rechthoekszijden a = ' + str(round(a, 2)) + ' en b = ' + str(round(b, 2)) + ' is de schuine zijde ' + str(round(c, 2)))
