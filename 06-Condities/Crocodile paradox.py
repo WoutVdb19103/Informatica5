fae2fcb6077864f8c0954b8aa1315b08e9a837ed
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2018-10-11 10:05:19 +0200

    Crocodile paradox

---
+++ 06-Condities/Crocodile paradox.py
@@ -0,0 +1,12 @@
+#invoer
+x = str(input('Wie heeft stalen zenuwen? '))
+
+#berekening
+if x == 'vader':
+    print('Krokodil geeft kind terug')
+
+elif x == 'krokodil':
+    print('Krokodil eet kind op')
+
+else:
+    print('Moe van het denken')
--- 06-Condities/Trolleyprobleem.py
+++ 06-Condities/Trolleyprobleem.py
@@ -3,17 +3,12 @@ hendel_trekken = str(input('Trek aan de hendel van de wissel? (ja/nee): '))
 man_op_spoor_duwen = str(input('Duw de man op het spoor? (ja/nee): '))
 
 #vragen
-if hendel_trekken == 'ja':
-    if man_op_spoor_duwen == 'ja':
-        doden = '2'
-    else:
-        doden = '1'
-
+if hendel_trekken == 'ja' and man_op_spoor_duwen == 'ja':
+    doden = 2
+elif hendel_trekken == 'nee' and man_op_spoor_duwen == 'nee':
+    doden = 5
 else:
-    if man_op_spoor_duwen == 'ja':
-        doden = '1'
-    else:
-        doden = '5'
+    doden = 1
 
 #uitvoer
 print(doden)
