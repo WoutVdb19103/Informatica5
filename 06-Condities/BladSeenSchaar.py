a3738047d4e29d4b5c6985dec9beb7fe450e01e3
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2018-10-23 15:58:37 +0200

    Risk

--- 06-Condities/BladSeenSchaar.py
+++ 06-Condities/BladSeenSchaar.py
@@ -3,7 +3,8 @@ speler_1 = str(input('Welke vorm achter je rug (blad/steen/schaar, speler 1)? ')
 speler_2 = str(input('Welke vorm achter je rug (blad/steen/schaar, speler 2)? '))
 
 #bewerking
-if speler_1 == 'blad' and speler_2 == 'schaar' or speler_1 == 'steen' and speler_2 == 'blad' or speler_1 == 'schaar' and speler_2 == 'steen':
+if speler_1 == 'blad' and speler_2 == 'schaar' or speler_1 == 'steen' and speler_2 == 'blad' or speler_1 == 'schaar' \
+and speler_2 == 'steen':
     uitkomst = 'speler 2 wint'
 
 if speler_1 == 'blad' and speler_2 == 'steen' or speler_1 == 'steen' and speler_2 == 'schaar' or speler_1 == 'schaar' and speler_2 == 'blad':
