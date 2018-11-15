ae2f241dfd1b24234f8c1e9264b5ab5a1cc8a7e2
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2018-11-15 09:08:34 +0100

    De rij van Fibonacci

--- 07-a-lteraties-For-lus/De rij van Fibonacci.py
+++ 07-a-lteraties-For-lus/De rij van Fibonacci.py
@@ -1,15 +1,11 @@
 #invoer
-x = int(input('Geef een getal: '))
+n = int(input('Hoeveelste getal van Fibonacci: '))
 
+vorige = 1
+huidige = 1
 #bewerking
-if x == 0:
-    print('0')
-elif x == 1 or x == 2:
-    print('1')
-elif x > 2:
-    def fib(n):
-        a,b = 1,1
-        for _ in range(n-1):
-            a,b = b,a+b
-        return a
-    print(fib(x))
+for i in range(n - 2):
+    vorige, huidige = huidige, huidige + vorige
+    #print(vorige, huidige)
+
+print(huidige)
