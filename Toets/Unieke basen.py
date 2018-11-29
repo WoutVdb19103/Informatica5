5853b6908729fbc7a44fd4f706a58fa975d1c4df
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2018-11-29 09:14:49 +0100

    Unieke basen (TOETS)

---
+++ Toets/Unieke basen.py
@@ -0,0 +1,27 @@
+#invoer
+n = int(input('Aantal basen? '))
+m, aantal_verschillende, basea, baset, baseg, basec = '', 0, '', '', '', ''
+
+#bewerking
+for i in range(n):
+    base = input('Welke base? ')
+
+    if base not in m:
+        m += base
+        aantal_verschillende += 1
+
+        if base == 'A':
+            basea += 'A'
+        elif base == 'T':
+            baset += 'T'
+        elif base == 'G':
+            baseg += 'G'
+        else:
+            basec += 'C'
+
+#uitvoer
+if aantal_verschillende == 1:
+    print('De DNA-keting bevat 1 soort base: ', base)
+
+else:
+    print('De DNA-keting bevat', aantal_verschillende, 'verschillende soorten basen: {} {} {} {}'.format(basea, basec, baseg, baset))
