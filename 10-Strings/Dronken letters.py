17f7e5f378ed49a1f294f33b8448243dccdeff64
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2019-01-17 09:53:15 +0100

    Dronken letters

---
+++ 10-Strings/Dronken letters.py
@@ -0,0 +1,23 @@
+# de eerste letter van het woord drinkt niet en neem je gewoon over.
+#alle letters op een even index groter dan 0 worden hoofdletters.
+#op een klinker (a, e, i, o of u) die in het nieuwe woord door regel 1 of 2 een hoofdletter geworden is, volgt altijd een hoofdletter.
+#alle andere letters vallen in slaap en worden of blijven kleine letters.
+
+def dronken_voeren(woord):
+    nieuw_woord = woord[0]
+
+    for i in range(1, len(woord)):
+
+            if i % 2 == 0:
+
+                nieuw_woord += woord[i].upper()
+
+            elif nieuw_woord[-1] in 'AEIOU':
+                nieuw_woord += woord[i].upper()
+
+            else:
+                nieuw_woord += woord[i].lower()
+
+    return nieuw_woord
+
+print(dronken_voeren('drinkeboer'))
