f49017f8dded8865659970a5b415cbbaf1296543
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2019-01-24 09:56:08 +0100

    Geen twee na elkaar

---
+++ Test 2/Geen twee na elkaar.py
@@ -0,0 +1,23 @@
+#def ontdubbelen(woord):
+#    nieuw_woord = ''
+#
+#    for letter in woord:
+#        if letter == woord[woord.find(letter) + 1]:
+#           woord = woord[:woord.find(letter) - 1] + woord[woord.find(letter) + 1:]
+#
+#    return woord
+#
+#print(ontdubbelen('kaass'))
+
+
+def ontdubbelen(woord):
+    nieuw_woord = woord[0]
+
+    for i in range(1, len(woord)):
+
+        if woord[i] != woord[i - 1]:
+            nieuw_woord += woord[i]
+
+    return nieuwe_woord
+
+print(ontdubbelen('kaassseeennn'))
