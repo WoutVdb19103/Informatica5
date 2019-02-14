d13b0f3aececc2610c970aa5504059d81c2a12a0
WoutVdb19103 <wout.vandenberghe@sgsintpaulus.eu>
2019-02-14 08:46:34 +0100

    Gotcha

--- 12a-Lists/Gotcha.py
+++ 12a-Lists/Gotcha.py
@@ -1,28 +1,43 @@
-def ik_heb_gemoord(list, moordenaar):
-    if len(list) == 1:
-        x = list[0]
-    elif list.index(moordenaar) + 1 >= len(list):
-        x = list[1]
-        list.pop(0)
-    elif list.index(moordenaar) + 2 >= len(list):
-        x = list[0]
-        list.pop(list.index(moordenaar) + 1)
-    else:
-        x = list[list.index(moordenaar) + 2]
-        list.pop(list.index(moordenaar) + 1)
+#def ik_heb_gemoord(list, moordenaar):
+#    if len(list) == 1:
+#        x = list[0]
+#    elif list.index(moordenaar) + 1 >= len(list):
+#        x = list[1]
+#        list.pop(0)
+#    elif list.index(moordenaar) + 2 >= len(list):
+#        x = list[0]
+#        list.pop(list.index(moordenaar) + 1)
+#    else:
+#        x = list[list.index(moordenaar) + 2]
+#        list.pop(list.index(moordenaar) + 1)
+#
+#    return (x, list)
+#
+#def ik_ben_vermoord(list, naam):
+#    if len(list) == 1:
+#        x = list[0]
+#    elif list.index(naam) == 0:
+#        x = list[1]
+#        list.pop(list.index(naam))
+#    elif list.index(naam) + 1 >= len(list):
+#        x = list[0]
+#        list.pop(list.index(naam))
+#    else:
+#        x = list[list.index(naam) + 1]
+#        list.pop(list.index(naam))
+#    return (x, list)
 
-    return (x, list)
+def ik_heb_gemoord(lijst, moordenaar):
+    #slachtoffer schrappen
+    index_moordenaar = lijst.index(moordenaar)
+    index_slachtoffer = (index_moordenaar + 1) % len(lijst)
 
-def ik_ben_vermoord(list, naam):
-    if len(list) == 1:
-        x = list[0]
-    elif list.index(naam) == 0:
-        x = list[1]
-        list.pop(list.index(naam))
-    elif list.index(naam) + 1 >= len(list):
-        x = list[0]
-        list.pop(list.index(naam))
-    else:
-        x = list[list.index(naam) + 1]
-        list.pop(list.index(naam))
-    return (x, list)
+    lijst[index_slachtoffer: index_slachtoffer + 1] = []
+
+    #nieuw doel moordenaar geven
+    index_moordenaar = lijst.index(moordenaar)
+    index_nieuw_doel = (index_moordenaar + 1) % len(lijst)
+
+    return lijst, lijst[index_nieuw_doel]
+
+print(ik_heb_gemoord(['jan', 'piet', 'joris', 'korneel'], 'korneel'))
--- 12a-Lists/Hoogtemeters.py
+++ 12a-Lists/Hoogtemeters.py
@@ -1,6 +1,12 @@
-def hoogtemeters(queue):
-    kaas = [' ']
+def hoogtemeters(list):
+    new_list = []
+    for h in list:
 
-    for :
+        while list.index(h) + 2 != len(list):
+
+            new_list += [list[h + 1]]
+
+    return list
 
 
+print(hoogtemeters([822, 61, 347, 234, 883, 336]))
